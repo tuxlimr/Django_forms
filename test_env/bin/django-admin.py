9fcfc1c517cd5349c3deb80cabb3353c0f1d8632
ajay <ajay.simplifyreality@gmail.com>
2020-02-02 11:18:28 +0530

    test_env added

---
+++ test_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/simplify/Downloads/personal_projects/Django_forms/test_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
